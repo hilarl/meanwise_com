d3baffeaf2c6048f288ab7975c12cf0ba68df37a
Hilal Agil <HiLAL@Hilals-MacBook-Pro.local>
2016-10-27 13:27:16 +0200

    updated ajax

--- subscribe.php
+++ subscribe.php
@@ -1,22 +1,22 @@
 <?php
 
-require("_mc.php");
+  require("_mc.php");
 
-use \DrewM\MailChimp\MailChimp;
+  use \DrewM\MailChimp\MailChimp;
 
-$MailChimp = new MailChimp('7d446f0db3c6f9e4b5f9b6f35a587fad-us14');
-$result = $MailChimp->post('lists/02801ed82a/members', array(
-                'email_address'     => $_POST["email"],
-                'status'            => 'subscribed'
-            ));
+  $MailChimp = new MailChimp('7d446f0db3c6f9e4b5f9b6f35a587fad-us10');
+  $result = $MailChimp->post('lists/02801ed82a/members', array(
+    'email_address'     => $_POST['email'],
+    'status'            => 'subscribed'
+  ));
 
-if($result){
-	$status = $result["status"];
+  if($result){
+  	$status = $result["status"];
 
-	if($status == "subscribed"){
-		echo "Great! We will keep you updated on our progress.";
-	}
-	else{
-		echo "Oops! Something went wrong. Please try again.";
-	}
-} 
+  	if($status == "subscribed"){
+  		echo "Great! We will keep you updated on our progress.";
+  	}
+  	else{
+  		echo "Oops! Something went wrong. Please try again.";
+  	}
+  }
