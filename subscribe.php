6a75f8d91952cdee5d3b7c9114c4053c58a4f9ce
Hilal Agil <HiLAL@Hilals-MacBook-Pro.local>
2016-10-27 15:15:35 +0200

    changed input name

--- subscribe.php
+++ subscribe.php
@@ -6,7 +6,7 @@
 
   $MailChimp = new MailChimp('7d446f0db3c6f9e4b5f9b6f35a587fad-us10');
   $result = $MailChimp->post('lists/02801ed82a/members', array(
-    'email_address'     => $_POST['email'],
+    'email_address'     => $_POST['emailAdd'],
     'status'            => 'subscribed'
   ));
 
